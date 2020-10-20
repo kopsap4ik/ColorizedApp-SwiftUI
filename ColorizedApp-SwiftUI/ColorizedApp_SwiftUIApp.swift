fb7331d8bcab395f4da4447d5a4fa8c242f0db95
Vasily Petuhov <vspetukhov@icloud.com>
2020-10-20 14:49:52 +0300

    Initial Commit

---
+++ ColorizedApp-SwiftUI/ColorizedApp_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorizedApp_SwiftUIApp.swift
+//  ColorizedApp-SwiftUI
+//
+//  Created by Василий Петухов on 20.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct ColorizedApp_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
