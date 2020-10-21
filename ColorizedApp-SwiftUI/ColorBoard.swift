ff0d32134326f972651394a43d664a6faa244495
Vasily Petuhov <vspetukhov@icloud.com>
2020-10-21 09:58:29 +0300

    finished HW2

---
+++ ColorizedApp-SwiftUI/ColorBoard.swift
@@ -0,0 +1,20 @@
+//
+//  ColorBoard.swift
+//  ColorizedApp-SwiftUI
+//
+//  Created by Василий Петухов on 20.10.2020.
+//
+
+import SwiftUI
+
+struct ColorBoard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ColorBoard_Previews: PreviewProvider {
+    static var previews: some View {
+        ColorBoard()
+    }
+}
---
+++ ColorizedApp-SwiftUI/SliderAndTexts.swift
@@ -0,0 +1,20 @@
+//
+//  SliderAndTexts.swift
+//  ColorizedApp-SwiftUI
+//
+//  Created by Василий Петухов on 21.10.2020.
+//
+
+import SwiftUI
+
+struct SliderAndTexts: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SliderAndTexts_Previews: PreviewProvider {
+    static var previews: some View {
+        SliderAndTexts()
+    }
+}
